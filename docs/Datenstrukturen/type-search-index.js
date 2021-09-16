95c1e98b6f768597381f1c7a1287e1cf79f98fcf
SushiWaUmai <eugene.bb1@gmail.com>
2021-09-16 22:02:47 +0200

    :books: update docs

--- docs/Datenstrukturen/type-search-index.js
+++ docs/Datenstrukturen/type-search-index.js
@@ -1,1 +1,1 @@
-typeSearchIndex = [{"l":"All Classes","u":"allclasses-index.html"},{"p":"<Unnamed>","l":"MergeSort"}];updateSearchResults();
+typeSearchIndex = [{"l":"All Classes","u":"allclasses-index.html"},{"p":"<Unnamed>","l":"BinarySearch"},{"p":"<Unnamed>","l":"MergeSort"},{"p":"<Unnamed>","l":"QuickSort"}];updateSearchResults();
