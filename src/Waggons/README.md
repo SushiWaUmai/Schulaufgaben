91c3ae914e67d3a16f1c9156e233ca059cce030c
SushiWaUmai <54822569+SushiWaUmai@users.noreply.github.com>
2021-09-30 08:51:15 +0200

    :zap: add waggon implementation

---
+++ src/Waggons/README.md
@@ -0,0 +1,11 @@
+# Waggons
+
+Beim Abschreiben:
+
+`package Waggon` am Anfang der Dateien loeschen.
+
+Und Kommentare selber umschreiben.
+
+Die Methoden `simuliere1()` und `simuliere2()` in der Klasse [Verkehr](Verkehr.java) umschreiben.
+
+Danke.
--- src/Waggons/Verkehr.java
+++ src/Waggons/Verkehr.java
@@ -2,25 +2,51 @@ package Waggons;
 
 public class Verkehr {
 
-    public static void main(String args[]){
-        test(); 
+    public static void main(String[] args) {
+        simuliere1();
+        simuliere2();
     }
-    
-    private static void test() {
+
+    public static void simuliere1() {
+        // Braucht man nicht
+        System.out.println("Simuliere 1...");
+
+        // Compiler Error
+        // Transportwagen wagen1 = new Transportwagen();
+       
+        Transportwagen wagen2 = new Gueterwagen();
+        Transportwagen wagen3 = new Flachwagen();
+
+        // Compiler Error
+        // Gueterwagen meinGW1 = new TransportWagen();
+
+        // Compiler Error
+        // Gueterwagen meinGW2 = new Flachwagen();
+
+        // Braucht man nicht
+        System.out.println("1 Simliert");
+    }
+
+    public static void simuliere2() {
+        // Braucht man nicht
+        System.out.println("Simuliere 2...");
+
         Transportwagen tw; Gueterwagen gw1, gw2; Flachwagen fw;
         gw1 = new Gueterwagen();
         tw = gw1;
+
+        // Compiler Error
         // gw2 = tw;
+
         gw2 = (Gueterwagen)tw;
+
+        // Compiler Error
         // fw = (Flachwagen)gw1;
-        fw = (Flachwagen)tw;
-    }
 
-    private static void simuliere1() {
-        
-    }
+        // Runtime Error
+        // fw = (Flachwagen)tw;
 
-    private static void simuliere2() {
-
+        // Braucht man nicht
+        System.out.println("1 Simliert");
     }
 }
